ebb1726be7dc0f23049d5155b7f1d61ee552ecd8
Riza Agustin <rizaagustin20@gmail.com>
2024-02-03 00:07:08 +0700

    menambahkan refresh token

---
+++ app/Http/Controllers/Api/RefreshController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RefreshController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $token = Auth::refresh();
+        // $user = Auth::refresh();
+        return response()->json([
+            'success' => true,
+            'user'    => auth()->guard('api')->user(),    
+            'token'   => $token   
+        ], 200);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', function (Request $request) {    
+        return $request->user();    
+    });    
+    Route::post('/refresh', App\Http\Controllers\Api\RefreshController::class)->name('refresh');
 });
+
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 
 
